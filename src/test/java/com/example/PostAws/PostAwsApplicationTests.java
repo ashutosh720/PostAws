772803dbd59f07e667dc37e9c4053e9b386a45b8
Ashutosh Rana <ashutoshrana1999@gmail.com>
2023-03-13 06:21:29 +0530

    jar added

---
+++ src/test/java/com/example/PostAws/PostAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PostAws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
